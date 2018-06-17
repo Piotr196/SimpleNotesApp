184e6461323c88406aa4c33067e983d22fca1727
andrzejczak-203847 <sendspamhere67@gmail.com>
2018-06-18 00:28:12 +0200

    Finished implementing NoteControllerTests

--- src/test/java/com/simplenotesapp/simplenotesapp/controller/NoteControllerTest.java
+++ src/test/java/com/simplenotesapp/simplenotesapp/controller/NoteControllerTest.java
@@ -3,8 +3,11 @@ package com.simplenotesapp.simplenotesapp.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.simplenotesapp.simplenotesapp.dto.NoteDto;
+import com.simplenotesapp.simplenotesapp.dto.NoteWithUsersDto;
+import com.simplenotesapp.simplenotesapp.dto.UserDto;
 import com.simplenotesapp.simplenotesapp.mapper.NoteDtoMapper;
 import com.simplenotesapp.simplenotesapp.mapper.NoteWithUsersDtoMapper;
 import com.simplenotesapp.simplenotesapp.model.Note;
@@ -12,7 +15,10 @@ import com.simplenotesapp.simplenotesapp.model.User;
 import com.simplenotesapp.simplenotesapp.service.NoteService;
 import com.simplenotesapp.simplenotesapp.service.SessionService;
 import com.simplenotesapp.simplenotesapp.service.UserService;
+import com.simplenotesapp.simplenotesapp.sorting.generic.SortingOrder;
+import com.simplenotesapp.simplenotesapp.sorting.notes.NotesSortingSubject;
 import org.apache.tomcat.jni.Error;
+import org.aspectj.weaver.ast.Not;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,15 +43,17 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.time.ZonedDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.core.Is.is;
@@ -56,13 +64,27 @@ import static org.hamcrest.core.Is.is;
 @WebAppConfiguration
 public class NoteControllerTest {
 
-    private static final NoteDto NOTE_DTO = new NoteDto(1L, "title", "content",
-            ZonedDateTime.now(), ZonedDateTime.now(), new HashSet<>());
+    private static final User USER = new User(1L, "login", "name", "surname",
+            "password", Sets.newHashSet());
 
-    private static final User USER = new User(1L, "login", "name", "surname", "password", Sets.newHashSet());
+    private static final UserDto USER_DTO = new UserDto(1L, "login", "name", "surname",
+            "password", Sets.newHashSet());
+
+    private static final NoteDto NOTE_DTO = new NoteDto(1L, "title", "content",
+            ZonedDateTime.now(), ZonedDateTime.now(), Sets.newHashSet(USER.getId()));
 
     private static final Note NOTE = new Note(1L, "title", "content",
-            ZonedDateTime.now(), ZonedDateTime.now(), new HashSet<>());
+            ZonedDateTime.now(), ZonedDateTime.now(), Sets.newHashSet(USER));
+
+    private static final List<Note> ALL_NOTES = Lists.newArrayList(NOTE);
+
+    private static final NoteWithUsersDto NOTE_WITH_USERS_DTO = new NoteWithUsersDto(1L, "title", "content",
+            ZonedDateTime.now(), ZonedDateTime.now(), Sets.newHashSet(USER_DTO));
+
+    static{
+        USER.getNotes().add(NOTE);
+        USER_DTO.getNotesId().add(NOTE.getId());
+    }
 
     private ObjectMapper mapper = new ObjectMapper();
 
@@ -93,39 +115,100 @@ public class NoteControllerTest {
         mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
         given(noteDtoMapper.mapToEntity(any(NoteDto.class))).willReturn(NOTE);
         given(noteDtoMapper.mapToDto(any(Note.class))).willReturn(NOTE_DTO);
+        given(noteWithUsersDtoMapper.mapToEntity(any(NoteDto.class))).willReturn(NOTE);
+        given(noteWithUsersDtoMapper.mapToDto(any(Note.class))).willReturn(NOTE_WITH_USERS_DTO);
         given(noteService.save(any(Note.class))).willReturn(NOTE);
+        given(noteService.update(any(Note.class))).willReturn(NOTE);
+        given(noteService.findAll(NotesSortingSubject.TITLE, SortingOrder.ASC)).willReturn(ALL_NOTES);
+        given(noteService.findOneById(any(Long.class))).willReturn(NOTE);
+        given(noteService.update(any(Note.class))).willReturn(NOTE);
+        given(noteService.sort(any(List.class), any(NotesSortingSubject.class), any(SortingOrder.class))).willReturn(ALL_NOTES);
+        given(sessionService.isUserInRole(any(String.class))).willReturn(true);
+        given(sessionService.getLoggedUser()).willReturn(USER);
+        given(userService.findOneById(any(Long.class))).willReturn(USER);
         mapper.registerModule(new JavaTimeModule());
     }
 
     @Test
     public void shouldAddNote() throws Exception {
+        //given
+        //when
         mvc.perform(post("/api/notes").with(csrf()).contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(NOTE_DTO))
-                .with(user("admin").roles("ADMIN", "USER"))
-        ).andExpect(status().isOk()).andExpect(jsonPath("$.id", is(1)));
+                .with(user("admin").roles("ADMIN", "USER"))).
+        //then
+                andExpect(status().isOk()).andExpect(jsonPath("$.id", is(1)));
     }
 
     @Test
-    public void deleteNote() {
+    public void shoulDeleteNote() throws Exception {
+        //given
+        //when
+        mvc.perform(delete("/api/notes/1").with(csrf())
+                .with(user("admin").roles("ADMIN", "USER"))).
+        //then
+                andExpect(status().isOk());
     }
 
     @Test
-    public void getNotes() {
+    public void shoulReturnUnauthorized() throws Exception {
+        //given
+        //when
+        when(sessionService.isUserInRole(any(String.class))).thenReturn(false);
+        mvc.perform(delete("/api/notes/1").with(csrf())
+                .with(user("user").roles("USER"))).
+        //then
+                andExpect(status().isUnauthorized());
     }
 
     @Test
-    public void getNote() {
+    public void shouldReturnNotes() throws Exception {
+        //given
+        //when
+        mvc.perform(get("/api/notes").with(csrf())
+                .with(user("admin").roles("ADMIN"))).
+        //then
+                andExpect(status().isOk());
     }
 
     @Test
-    public void updateNote() {
+    public void shouldReturnNote() throws Exception {
+        //given
+        //when
+        mvc.perform(get("/api/notes/1").with(csrf())
+                .with(user("user").roles("USER"))).
+        //then
+        andExpect(status().isOk()).andExpect(jsonPath("$.id", is(1)));
     }
 
     @Test
-    public void getUserLoginsForNote() {
+    public void updateNote() throws Exception {
+        //given
+        //when
+        mvc.perform(put("/api/notes").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(NOTE_DTO))
+                .with(user("user").roles("USER"))).
+        //then
+        andExpect(status().isOk()).andExpect(jsonPath("$.id", is(1)));
     }
 
     @Test
-    public void getNotesForUser() {
+    public void getUserLoginsForNote() throws Exception {
+        //given
+        //when
+        mvc.perform(get("/api/notes/1/users").with(csrf())
+                .with(user("user").roles("USER"))).
+        //then
+        andExpect(status().isOk()).andExpect(jsonPath("$[0]", is("login")));
+    }
+
+    @Test
+    public void getNotesForUser() throws Exception {
+        //given
+        //when
+        mvc.perform(get("/api/notes/users/1?sortby=TITLE&order=ASC").with(csrf())
+                .with(user("user").roles("USER"))).
+        //then
+        andExpect(status().isOk()).andExpect(jsonPath("$[0].id", is(1)));
     }
 }
